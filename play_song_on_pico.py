68de0750b83d2d826d854a3efe1846e5d544e362
afjones-code <afjones@bu.edu>
2025-09-14 22:07:01 -0400

    Update play_song_on_pico.py

--- play_song_on_pico.py
+++ play_song_on_pico.py
@@ -10,7 +10,7 @@ buzzer.duty_u16(0)
 
 def play_song_on_pico(digital_values, digital_length, note_duration=0.5):
    
-    for f in freq_list:
+    for f in digital_values:
         if f <= 0:
             buzzer.duty_u16(0)  # silence if freq <= 0
         else:
