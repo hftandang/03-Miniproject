bfef0f4380f569ed0e669dddeeabb4e48e1eb114
JustNasci <jusnasci@bu.edu>
2025-09-15 00:16:06 -0400

    Added raw_data_from_pico and send_song_to_pico

--- src/conductor.py
+++ src/conductor.py
@@ -43,6 +43,52 @@ SONG = [
 
 # --- Conductor Logic ---
 
+""" Code Written By Justin """
+def raw_data_from_pico(ip):
+    """Obtains raw data from a pico by pulling data from it """
+    healthUrl = f"http://{ip}/health"
+    dataUrl = f"http://{ip}/sensor"
+    try:
+        healthResponse = requests.get(healthUrl, timeout=0.1) # Pulls request data from sensor
+        healthData = healthResponse.json()                    # Gets JSON data from the request
+        if healthData["status"] != "ok":                      # If the device isn't ok, print health data
+            print("Status:", healthData["status"])
+            print("Device ID:", healthData["device_id"])
+            print("API Version:", healthData["api"])
+    except requests.exceptions.Timeout:
+        # This is expected, we can ignore it
+        pass
+    except requests.exceptions.RequestException as e:
+        print(f"Error contacting {ip}: {e}")
+
+    # For a time period of collectionDuration seconds, tries to obtain sensor info from the pico through looping for a certain duration
+    sensorDataArray = []
+    startTime = time.time()
+    collectionDuration = 10
+    timeoutInterval = 0.1
+
+    print(f"Collecting sensor data for {collectionDuration} data points...")
+    while (time.time() - startTime) < collectionDuration:
+        try:
+            sensorResponse = requests.get(dataUrl, timeout=0.1)
+            data = sensorResponse.json()
+            sensorDataArray.append(data)
+
+        except requests.exceptions.RequestException as e:
+            print(f"Error contacting {ip}: {e}")
+
+        time.sleep(timeoutInterval)
+
+    return sensorDataArray, len(sensorDataArray)
+
+
+# Sends the song to all the picos using the digitalvalues, digital length method
+def send_song_to_pico(digital_values, digital_length):
+    # Loops through all of the values in digital_values, and send them one-by-one to the Picos
+    for i in range(digital_length):
+        play_note_on_all_picos(digital_values[i],0.5)
+
+
 
 def play_note_on_all_picos(freq, ms):
     """Sends a /tone POST request to every Pico in the list."""
