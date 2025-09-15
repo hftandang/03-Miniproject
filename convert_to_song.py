7967734a8c2d870e502850339a70ab0214d70580
ajc03 <124468388+ajc03@users.noreply.github.com>
2025-09-15 07:52:56 -0400

    Update convert_to_song.py
    Added some comments to better explain the code to those who have never seen it before.

--- convert_to_song.py
+++ convert_to_song.py
@@ -1,4 +1,4 @@
-def convert_to_song(analog_values, analog_length):
+def convert_to_song(analog_values, analog_length): # Function expects 2 inputs, a list of analog values and the analog length (which should be equal to the analog values list's length)
 
     # Used Hertz for middle octive keys from link on Github
     C = 262.0
@@ -9,8 +9,9 @@ def convert_to_song(analog_values, analog_length):
     A = 440.0
     B = 494.0
 
-    digital_values = [None] * analog_length # Empty python list for the digital values
+    digital_values = [None] * analog_length # Empty python list for the digital values that is equal in length to the original analog list's length
 
+    # For loop for converting each value in the analog_values list into a note equivalent and saving that in the respective digital_values list spot
     for i in range(analog_length):
         if analog_values[i] <= 36.0:
             digital_values[i] = C
@@ -29,6 +30,7 @@ def convert_to_song(analog_values, analog_length):
 
     digital_length = len(digital_values) # Length of the new digital_values list (should be identical to the original length)
 
-    return digital_values, digital_length
+    return digital_values, digital_length # Return both the digital_values list and the new digital length (which should be identical to the original analg length)
+
 
 
