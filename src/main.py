18eba25799ad661bba7f4d6c38de94ae6e12e0fe
JustNasci <jusnasci@bu.edu>
2025-09-15 21:59:34 -0400

    added Hannah's code to main.py

--- src/main.py
+++ src/main.py
@@ -95,6 +95,19 @@ def map_value(x, in_min, in_max, out_min, out_max):
     return (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
 
 
+""" Hannah's Code """
+def collect_pico_data(record_button_stat):
+    intensity_array = []
+    # Read current sensor value, ranges from 0 to 65535, 0 being low voltage 65535 meaning high voltage around 3.3V, as voltage increases so does intensity of light
+    if record_button_stat:
+        start_time = time.ticks_ms()
+        while time.ticks_diff(time.ticks_ms(), start_time) < 10_000:
+            light_value = photo_sensor_pin.read_u16()
+            intensity_array.append(light_value)
+            time.sleep(0.05)  # clock is every 50ms
+    return intensity_array, len(intensity_array)
+
+
 async def handle_request(reader, writer):
     """Handles incoming HTTP requests."""
     global api_note_task
